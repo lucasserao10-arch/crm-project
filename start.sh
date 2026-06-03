320ddea78c09bd4eee4e8bbd34ec5fa998c758ff
Lucas Serao <lucas.serao.10@gmail.com>
2026-06-03 14:15:20 -0700

    fix ts-node path in startup seed

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 echo "Running database setup..."
 node /app/node_modules/prisma/build/index.js db push --skip-generate
 echo "Running seed..."
-node /app/node_modules/prisma/build/index.js db seed
+node /app/node_modules/ts-node/dist/bin.js --compiler-options '{"module":"CommonJS"}' /app/prisma/seed.ts
 echo "Starting Next.js..."
 exec node server.js
